dd48406867f6dd76a28fffa9b7d832854cc53e24
Juan <valenciavjuandavid@gmail.com>
2024-04-01 15:29:56 -0500

    Lista de municipios V2

--- resources/views/Municipios/new.blade.php
+++ resources/views/Municipios/new.blade.php
@@ -8,10 +8,37 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado  Municipio</title>
+    <title>Añadir Muniipio</title>
   </head>
   <body>
-    <h1>Listado de municipios</h1>
+    <h1>Añadir Municipio</h1>
+    <form method="POST" action="{{ route('municipios.store') }}">
+        @csrf
+  <div class="mb-3">
+    <label for="id" class="form-label">Codigo</label>
+    <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
+    <div id="idHelp" class="form-text">Codigo de Municipio</div>
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Municipio</label>
+    <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
+    name="name" placeholder="Nombre del Municipio">
+  </div>
+
+  <label for="Departamento">Departamento:</label>
+    <select class="form-select" id="Departamento" name="code" required>
+    <option selected disabled value="">Choose one...</option>
+    @foreach ($municipios as $municipio)
+    <option value="{{ $municipio->muni_codi }}">{{$municipio->muni_nomb }}</option>
+    @endforeach
+</select>
+  </div>
+  <div class="mt-3">
+  <button type="submit" class="btn btn-primary">Save</button>
+  <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
+</div>
+</form>
+</div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -23,32 +50,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
-  <body>
-    <div class="container">
-        <h1>Lista de Municipios</h1>
-     
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Code</th>
-                    <th scope="col">Municipio</th>
-                    <th scope="col">Departamento</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($municipios as $municipio)
-                <tr>
-                    <th scope="row">{{$municipio->muni_codi}}</th>
-                    <td>{{ $municipio->muni_nomb }}</td>
-                    <td>{{ $municipio->depa_nomb }}</td>
-                    <td>
-                     
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-</table>
   </body>
 </html>
