3e431ee8add091713c4b0c73a69b05c5dac6f3b4
Juan <vjuandavid03@gmail.com>
2024-03-31 22:53:50 -0500

    "Crear Comunas"

--- resources/views/comunas/new.blade.php
+++ resources/views/comunas/new.blade.php
@@ -8,10 +8,37 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado de Comunas</title>
+    <title>Add Comuna</title>
   </head>
   <body>
-    <h1>Listado de Comunas</h1>
+    <h1>Add Comuna</h1>
+    <form method="POST" action="{{ route('comunas.store') }}">
+        @csrf
+  <div class="mb-3">
+    <label for="id" class="form-label">Code</label>
+    <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
+    <div id="idHelp" class="form-text">Commune code</div>
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Commune</label>
+    <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
+    name="name" placeholder="comuna name.">
+  </div>
+
+  <label for="municipality">Municipality:</label>
+    <select class="form-select" id="municipality" name="code" required>
+    <option selected disabled value="">Choose one...</option>
+    @foreach ($municipios as $municipio)
+    <option value="{{ $municipio->muni_codi }}">{{$municipio->muni_nomb }}</option>
+    @endforeach
+</select>
+  </div>
+  <div class="mt-3">
+  <button type="submit" class="btn btn-primary">Save</button>
+  <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
+</div>
+</form>
+</div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -23,30 +50,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
-  <body>
-    <div class="container">
-        <h1>Commune List</h1>
-     <a href="{{ route('comunas.create')}}" class= "btn btn-sucess">Add</a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Code</th>
-                    <th scope="col">Commune</th>
-                    <th scope="col">Municipality</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($comunas as $comuna)
-                <tr>
-                    <th scope="row">{{$comuna->comu_codi}}</th>
-                    <td>{{ $comuna->comu_nomb }}</td>
-                    <td>{{ $comuna->muni_nomb }}</td>
-                    <td><span>Actions</span></td>
-                </tr>
-                @endforeach
-            </tbody>
-</table>
   </body>
 </html>
